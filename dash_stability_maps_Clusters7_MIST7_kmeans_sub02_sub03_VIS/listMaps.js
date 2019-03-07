5858b6754d5ece40c528f3d485fac6e65f7c2643
bamal <boukhdhiramaal@gmail.com>
2019-03-06 22:57:10 -0500

    Add dashboards

---
+++ dash_stability_maps_Clusters7_MIST7_kmeans_sub02_sub03_VIS/listMaps.js
@@ -0,0 +1,6 @@
+var listMaps = [ "Proba map::sub02::Network:VIS::State:1" ,"Proba map::sub02::Network:VIS::State:2" ,"Proba map::sub03::Network:VIS::State:1" ,"Proba map::sub03::Network:VIS::State:2" ];
+var params = {};
+params[0] = { origin: {X: 98.000000, Y: 134.000000, Z:72.000000}, voxelSize: 1.000000, nbSlice: {Y: 233, Z: 189}, min: 0.300000, max: 1.000000};
+params[1] = { origin: {X: 98.000000, Y: 134.000000, Z:72.000000}, voxelSize: 1.000000, nbSlice: {Y: 233, Z: 189}, min: 0.300000, max: 1.000000};
+params[2] = { origin: {X: 98.000000, Y: 134.000000, Z:72.000000}, voxelSize: 1.000000, nbSlice: {Y: 233, Z: 189}, min: 0.300000, max: 1.000000};
+params[3] = { origin: {X: 98.000000, Y: 134.000000, Z:72.000000}, voxelSize: 1.000000, nbSlice: {Y: 233, Z: 189}, min: 0.300000, max: 1.000000};
